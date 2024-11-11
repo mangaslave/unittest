36aee03e646602a88bd597f96fc7ac59abf899c9
adosha o <adoshao@adoshas-Air.home>
2024-11-11 09:44:37 -0800

    initial commit

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 ////The environment variable for your connection string goes here////
-// let mongoDB = environmentvariablegoeshere;
+let mongoDB = process.env.MONGODB;
 // Make sure to create .env file and use an environment variable
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
