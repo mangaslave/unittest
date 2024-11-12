263f7d1a85274f4f9d29ba3168c7b572927d29d4
adosha o <adoshao@adoshas-Air.home>
2024-11-11 21:44:05 -0800

    Add unit test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+jest.mock('mongoose', () => ({
+  connect: jest.fn(),
+  connection: {
+    on: jest.fn(),
+  },
+}));
 
 test('renders GetToDoItem component', () => {
     render(<App />);
